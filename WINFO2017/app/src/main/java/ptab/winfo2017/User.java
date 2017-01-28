68e48b9881e803fbb53c19e8d80e5e4d772a0882
tyler fost <fosty@cs.washington.edu>
2017-01-28 11:02:10 -0800

    created inital user framework

---
+++ WINFO2017/app/src/main/java/ptab/winfo2017/User.java
@@ -0,0 +1,36 @@
+package ptab.winfo2017;
+
+/**
+ * Created by Tyler on 1/28/2017.
+ */
+
+import java.util.List;
+
+/**
+ * A User is someone who uses this app. Each user contains information such as User ID,
+ * Contact information, list of previously visited locations, etc.
+ */
+public class User {
+
+    public User() {}
+
+    /**
+     *
+     * @return the user's name
+     */
+    public String getName() {
+        return "";
+    }
+
+    /**
+     * Returns the user's id, for use in uniquely identifying the user.
+     * @return an integer representation of this user.
+     */
+    public int getUserID() {
+        return -1;
+    }
+
+    public List<EmergencyContact> getEmergencyContacts() {
+        return null;
+    }
+}
